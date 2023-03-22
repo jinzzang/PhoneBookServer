bc73e358bbeaaa466add3ad75975172e66487b79
junsupark <wnstn6764@naver.com>
2023-03-22 21:16:56 +0900

    if 문을 2진수로 표현

--- src/main/java/com/example/phoneBook/Controller.java
+++ src/main/java/com/example/phoneBook/Controller.java
@@ -37,14 +37,17 @@ public class Controller {
         final var n = Optional.ofNullable(name);
         final var num = Optional.ofNullable(number);
 
-        if(n.isPresent() && num.isPresent()) {
-            var n_ = filter(c -> c.getName().equals(name) && c.getNumber().equals(number), pn);
-            return n_.toString();
-        } else if(n.isPresent() && !num.isPresent()) {
-            return filter(c -> c.getName().equals(name), pn).toString();
-        } else if(!n.isPresent() && num.isPresent()) {
-            return filter(c -> c.getNumber().equals(number), pn).toString();
-        } else throw new IllegalArgumentException("검색할 이름 또는 전화번호를 입력하지 않았습니다.");
+        final var p = 2 * (n.isPresent() ? 1 : 0) + (num.isPresent() ? 1 : 0);
+
+        switch (p) {
+            case 0b00: throw new IllegalArgumentException("검색할 이름 또는 전화번호를 입력하지 않았습니다.");
+            case 0b01: return filter(c -> c.getNumber().equals(number), pn).toString();
+            case 0b10: return filter(c -> c.getName().equals(name), pn).toString();
+            case 0b11: 
+                var n_ = filter(c -> c.getName().equals(name) && c.getNumber().equals(number), pn);
+                return n_.toString();
+            default: throw new UnsupportedOperationException("있을 수 없는 경우입니다.");
+        }
     }
 
     @GetMapping("/deleteAll")
@@ -55,18 +58,24 @@ public class Controller {
         final var n = Optional.ofNullable(name);
         final var num = Optional.ofNullable(number);
 
-        if(n.isPresent() && num.isPresent()) {
-            pn = filter(c -> !c.getName().equals(name) && !c.getNumber().equals(number), pn);
-            return name + " : " + number + " 이(가) 삭제되었습니다.";
-        } else if(!n.isPresent() && num.isPresent()) {
-            final var n_ = filter(c -> c.getNumber().equals(number), pn);
-            pn = filter(c -> !c.getNumber().equals(number), pn);
-            return n_.get(0) + " 이(가) 삭제되었습니다.";
-        } else if(n.isPresent() && !num.isPresent()) {
-            final var n_ = filter(c -> c.getName().equals(name), pn);
-            pn = filter(c -> !c.getName().equals(name), pn);
-            return n_.get(0) + " 이(가) 삭제되었습니다.";
-        } else throw new IllegalArgumentException("삭제할 이름 또는 번호를 입력하지 않았습니다.");
+        final var p = 2 * (n.isPresent() ? 1 : 0 ) + (num.isPresent() ? 1 : 0);
+
+        switch (p) {
+            case 0b00: throw new IllegalArgumentException("삭제할 이름 또는 번호를 입력하지 않았습니다.");
+            case 0b01: 
+                final var n_ = filter(c -> c.getNumber().equals(number), pn);
+                pn = filter(c -> !c.getNumber().equals(number), pn);
+                return n_.get(0) + " 이(가) 삭제되었습니다.";
+            case 0b10: 
+                final var n__ = filter(c -> c.getName().equals(name), pn);
+                pn = filter(c -> !c.getName().equals(name), pn);
+                return n__.get(0) + " 이(가) 삭제되었습니다.";
+            case 0b11: 
+                pn = filter(c -> !c.getName().equals(name) && !c.getNumber().equals(number), pn);
+                return name + " : " + number + " 이(가) 삭제되었습니다.";
+        
+            default: throw new UnsupportedOperationException();
+        }
     }
 
     private static <T> ArrayList<T> filter(Function<T, Boolean> p, ArrayList<T> list) {
@@ -81,36 +90,6 @@ public class Controller {
             } return result;
         } 
     }
-
-    // @GetMapping("/deleteFirst")
-    // public String del(
-    //     @RequestParam(name = "name", required = false) String name,
-    //     @RequestParam(name = "number", required = false) String number
-    // ) {
-    //     final var n = Optional.ofNullable(name);
-    //     final var num = Optional.ofNullable(number);
-
-    //     if(!deleteAll && n.isPresent() && num.isPresent()) { // bool def = false
-    //         pn = filter(c -> !c.getName().equals(name) && !c.getNumber().equals(number), pn);
-    //         return name + " : " + number + " 이(가) 삭제되었습니다.";
-    //     } else if(!deleteAll && n.isPresent() && num.isEmpty()) {
-    //         final var names = filter(n_ -> n_.getName().equals(name), pn);
-    //         return names + " 검색된 연락처 중 삭제할 연락처의 번호를 입력하세요.";
-    //     } else if(!deleteAll && n.isEmpty() && num.isPresent()) {
-    //         pn = filter(c -> !c.getNumber().equals(number), pn); 
-    //         return number + " 이(가) 삭제되었습니다.";
-    //     } else if(deleteAll && n.isPresent() && num.isPresent()) {
-    //         pn = filter(n_ -> !n_.getName().equals(name), pn);
-    //         return name + " : " + number + "이(가) 삭제되었습니다.";
-    //     } else if(deleteAll && n.isPresent() && num.isEmpty()) {
-    //         pn = filter(n_ -> !n_.getName().equals(name), pn);
-    //         return name + " 이(가) 삭제되었습니다.";
-    //     } else if(deleteAll && n.isEmpty() && num.isPresent()) {
-    //         final var ls = filter(n_ -> n_.getNumber().equals(number), pn);
-    //         pn = filter(n_ -> !n_.getNumber().equals(number), pn);
-    //         return ls + " 이(가) 삭제되었습니다.";
-    //     } else throw new IllegalArgumentException("삭제할 이름 또는 번호를 입력하지 않았습니다.");
-    // }
 }
 
 /*
