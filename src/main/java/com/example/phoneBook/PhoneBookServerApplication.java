3098d1a7264f62badf6bde4e9d2a6cb9eaa4418a
Cho Jin Ho <whwlsgh1111@naver.com>
2023-10-06 10:34:12 +0900

    done

--- src/main/java/com/example/phoneBook/PhoneBookServerApplication.java
+++ src/main/java/com/example/phoneBook/PhoneBookServerApplication.java
@@ -2,10 +2,16 @@ package com.example.phoneBook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PhoneBookServerApplication {
+public class PhoneBookServerApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PhoneBookServerApplication.class);
+	}
 	public static void main(String[] args) {
 		
 		SpringApplication.run(PhoneBookServerApplication.class, args);
